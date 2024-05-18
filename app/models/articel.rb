18ab5aa9b8ff5ca00abb0328251f7d02052b33a3
bamboo <bond007blues@gmail.com>
2024-05-18 16:11:50 +0900

    Articleモデルのつづりを修正して再作成

--- app/models/articel.rb
+++
@@ -1,2 +0,0 @@
-class Articel < ApplicationRecord
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_12_014235) do
+ActiveRecord::Schema.define(version: 2024_05_18_065704) do
 
-  create_table "articels", force: :cascade do |t|
+  create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.text "contet"
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
