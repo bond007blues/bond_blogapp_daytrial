c98608fb920d428a87174aa48d4c4fe6853824ae
bamboo <bond007blues@gmail.com>
2024-05-20 23:32:12 +0900

    編集画面を作成。idを指定して開けるように

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,7 +22,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @artile = Article.find(params[:id])
+    @article = Article.find(params[:id])
+  end
+  
+  def update
   end
 
   private
